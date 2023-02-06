0b24c96ebd3cb09d1ba3d6c832f03517a1ad872c
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-06 18:36:15 +0000

    Ref(main):  from static using of range

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import { Observable, fromEvent } from 'rxjs';
+import { Observable, of, range } from 'rxjs';
 
 const observer = {
   next: (value: any) => console.log('next', value),
@@ -8,29 +8,9 @@ const observer = {
 };
 
 
-/*
- *  Create streams from events, given target and event name.
- */
-const source$: Observable<any> = fromEvent(document, 'keyup');
+const source$: Observable<any> = of(1,2,3,4,5);
 
-/*
- *  Each subscription creates it's own execution path between
- *  observable and observer (also known as unicasting). So, in this case,
- *  every subscription will wire up a new event listener.
- */
-const subOne = source$.subscribe(observer);
-const subTwo = source$.subscribe(observer);
-
-
-setTimeout(() => {
-  /*
-   *  For long running observables we need to make sure to clean
-   *  them up when we are finished to prevent memory leaks and
-   *  unintended behavior. In this case, we are cleaning up
-   *  one subscription but not the other, leaving it active.
-   *  We will learn different techniques to automate this
-   *  process in an upcoming lesson.
-   */
-  console.log('unsubscribing');
-  subOne.unsubscribe();
-}, 3000);
+console.log('proving');
+source$.subscribe(observer);
+range(1,5).subscribe(observer);
+console.log('this is synchronous');
