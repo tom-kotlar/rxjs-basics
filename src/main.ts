187ecbbca71a08d7dc5ee7786ad45424658e99ec
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-07 11:06:21 +0000

    Ref(main): Interval, Timer

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,29 @@
 import './style.css'
-import { Observable, from, of, range } from 'rxjs';
-
-const observer = {
-  next: (value: any) => console.log('next', value),
-  error: (error: any) => console.log('error', error),
-  complete: () => console.log('complete!')
-};
+import { interval, timer } from 'rxjs';
 
 /*
- * from can turn nearly anything into an observable
- * When from receieves an array, it loops through each item
- * within that array, emitting them in sequence.
+ * interval emits numbers in sequence based on the
+ * duration that you specify. In this case, a number
+ * will be emitted every 1000ms (1s)
  */
-
-from([1, 2, 3, 4, 5]).subscribe(console.log);
+const interval$ = interval(1000);
 
 /*
- * This works for any array like object as well, for instance, 
- * when from receieves a string (which has a length property) 
- * it will loop through emitting each character.
+ * We'll just supply a function for next in this case,
+ * rather than observer object.
  */
-from('Hello').subscribe(console.log);
+interval$.subscribe(console.log);
 
 /*
- * When from receieves a promise, it will call .then, emitting
- * the response. We will see ways to make requests using an
- * observable interface in upcoming lessons, but for now we will
- * just use fetch.
+ * If you need the first item to be emitted on an interval
+ * different than the rest, you can use the timer operator instead.
+ * For example, let's have the first item emit immediately, followed
+ * by a value every 1000ms after.
  */
-from(fetch('https://api.github.com/users/octocat')).subscribe(console.log);
-
-function* hello() {
-    yield 'Hello';
-    yield 'World';
-};
-
-const iterator = hello();
+ const timer$ = timer(0, 1000);
 
 /*
- * When from receieves a iterator it will drop it in a do while loop,
- * calling .next and emitting each item until there are no more items left.
+ * You can also emit a single item after a specified duration, then complete,
+ * by just supplying the first argument.
  */
-from(iterator).subscribe(console.log);
+//  const timer$ = timer(1000);
