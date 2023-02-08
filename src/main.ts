5483cbe3deba9d5130f81783b18d9e093a2e485c
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-08 17:55:10 +0000

    Ref(main): Sample, Sampletime

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,38 @@
 
-import { asyncScheduler, debounce, debounceTime, distinctUntilChanged, fromEvent, interval, map, tap, throttleTime } from 'rxjs';
+import { Observable, fromEvent, interval, map, sample, sampleTime, tap } from 'rxjs';
 import './style.css'
 
-const click$ = fromEvent(document, 'click')
+const click$: Observable<any>  = fromEvent(document, 'click')
+const timer$: Observable<number> = interval(1000);
 
 
+click$.pipe(
 
-// click$.pipe(
-//      /*
-//       * throttleTime will emit the first value, then ignore
-//       * values for the specified duration. After that duration
-//       * has passed, the next value from the source will be
-//       * emitted, with the previous behavior repeated.
-//       */
-//     throttleTime(3000)
-// ).subscribe(console.log)
-
-function calculateScrollPercent(element: { scrollTop: any; scrollHeight: any; clientHeight: any; }) {
-    const { scrollTop, scrollHeight, clientHeight } = element;
-  
-    return (scrollTop / (scrollHeight - clientHeight)) * 100;
-  }
-  
-  // elems
-  const progressBar: any = document.querySelector('.progress-bar');
-  
-  // streams
-  const scroll$ = fromEvent(document, 'scroll');
-  
-  const progress$ = scroll$.pipe(
-    /*
-     * For extremely active streams like scroll events,
-     * throttleTime can be used to limit the number of emitted
-     * values. In this case, we'll just update our scroll bar every
-     * 30ms of scrolling.
+        /*
+     * At the duration you specify, sample time will emit the last
+     * emitted value within that window. For instance, in this 
+     * example we are sampling at an interval of 4s. When the 4s
+     * interval timer begins, you can click twice. Once 4s passes,
+     * the second click will be emitted. This behavior is then repeated.
+     * If no values are emitted from the source in the sample
+     * window no values are emitted by sampleTime.
      */
-    throttleTime(30, asyncScheduler, {
-        leading: false,
-        trailing: true
-    }),
+    sampleTime(4000), 
+    tap(console.log),
+    map(({ clientX, clientY })  => ({
+        clientX,
+        clientY
+      }))
+).subscribe(console.log)
+
+
+
+timer$.pipe(
     /*
-     * For every scroll event, we use our helper function to 
-     * map to a current scroll progress value.
+     * The sample window can also be based off another stream. 
+     * For instance, in this example every time you click the
+     * last value emitted by the timer$ observable will be emitted
+     * by sample.
      */
-    map(({ target }: any) => calculateScrollPercent(target.documentElement)),
-    tap(console.log)
-  );
-  /*
-   * We can then take the emitted percent and set the width
-   * on our progress bar.
-   */
-  progress$.subscribe(percent => {
-    progressBar.style.width = `${percent}%`;
-  });
+    sample(click$)
+  ).subscribe(console.log);
