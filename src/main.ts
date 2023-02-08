be9827b886d109910f5cc640c5edd388f1019ce9
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-08 18:01:39 +0000

    Ref(main): auditTime - clickEvent

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,29 @@
 
-import { Observable, fromEvent, interval, map, sample, sampleTime, tap } from 'rxjs';
+import { Observable, auditTime, fromEvent, map } from 'rxjs';
 import './style.css'
 
 const click$: Observable<any>  = fromEvent(document, 'click')
-const timer$: Observable<number> = interval(1000);
-
-
-click$.pipe(
-
-        /*
-     * At the duration you specify, sample time will emit the last
-     * emitted value within that window. For instance, in this 
-     * example we are sampling at an interval of 4s. When the 4s
-     * interval timer begins, you can click twice. Once 4s passes,
-     * the second click will be emitted. This behavior is then repeated.
-     * If no values are emitted from the source in the sample
-     * window no values are emitted by sampleTime.
-     */
-    sampleTime(4000), 
-    tap(console.log),
-    map(({ clientX, clientY })  => ({
-        clientX,
-        clientY
-      }))
-).subscribe(console.log)
 
 
 
-timer$.pipe(
+click$
+  .pipe(
     /*
-     * The sample window can also be based off another stream. 
-     * For instance, in this example every time you click the
-     * last value emitted by the timer$ observable will be emitted
-     * by sample.
+     * auditTime will begin window when the source emits. Then,
+     * once the window passes, the last emitted value
+     * from the source will be emitted. For instance, in this
+     * example if you click a 4s timer will be started. 
+     * At the end, the last click event during that window
+     * will be emitted by auditTime. This is similar to the
+     * behavior of throttleTime, if you were to pass in a config
+     * to emit the value on the trailing edge.
      */
-    sample(click$)
-  ).subscribe(console.log);
+    auditTime(4000),
+    /*
+     * adding mapping to stackblitz example since logging
+     * raw events is flaky
+     */
+    
+    map(({clientX, clientY}) => ({clientX, clientY}))
+  )
+  .subscribe(console.log);
