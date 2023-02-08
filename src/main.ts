084b9327a342fe78284b5786dd7f4096eb01d3a8
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-08 14:03:22 +0000

    Ref(main): Take, First

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,38 @@
+import { Observable, first, fromEvent, map, of, take } from 'rxjs'
 import './style.css'
 
-import { interval } from 'rxjs';
-import { scan, map, filter } from 'rxjs/operators';
 
-// elem refs
-const countdown: any = document.getElementById('countdown');
-const message: any = document.getElementById('message');
 
-// streams
-const counter$ = interval(1000);
-
-counter$.pipe(
-    scan((accumulator: any, current: any) => {
-return accumulator - 1
-    }, 10),
-filter(value => value >= 0)
-).subscribe((value: any) => {
-    countdown.innerHTML = value
-    if (!value ) {
-        message.innerHTML = "LiftOff"
-    }
-})
+const numbers$:Observable<number> = of(1,2,3,4,5)
+const clicks$: Observable<Event> = fromEvent(document, 'click')
 
 /*
- * Starting countdown example. In future lessons we will learn
- * about how to seed our countdown, complete when the timer hits zero, 
- * pause the countdown, and resume. More to come!
+ * take emits the first x values from the source,
+ * then completes. In this case, 1,2,3 will be emitted.
  */
 
+numbers$.pipe(take(3)).subscribe({
+    next: console.log,
+    complete: () => console.log('Complete!')
+  });
+  
+/*
+ * In this example, we will take the first value that matches
+ * the provided criteria before completing. While we could use
+ * a combination of filter(condition) and take(1), we can also 
+ * use the first operator to fulfill the same use case. 
+ * If you supply no values to first, it is equivalent to take(1).
+ */
 
-
+clicks$.pipe(
+        map((event: any) => ({
+          x: event.clientX,
+          y: event.clientY
+        })),
+        // like filter(condition), take(1)
+         first(({ y }) => y > 200)
+      )
+.subscribe({
+    next: console.log,
+    complete: () => console.log('Complete')
+})
