7740e276ba98beb591c4da7f7ecabfd22b54c02d
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-09 11:24:17 +0000

    Ref(*): Flatten inner observables mergeMap

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,48 @@
 
-import { debounceTime, fromEvent, map, mergeAll, mergeMap} from 'rxjs';
+import { fromEvent, interval, map, mergeMap, takeUntil } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import './style.css'
 
-const textInput: any = document.getElementById('text-input')
+const mousedown$ = fromEvent(document, 'mousedown')
+const mouseup$ = fromEvent(document, 'mouseup')
 
-const input$ = fromEvent(textInput, 'keyup')
 
-// input$.pipe(
-//   map((event: any) => {
-//     const term = event.target.value
+const interval$ = interval(1000)
+const click$ = fromEvent(document, 'click');
 
-//     return ajax.getJSON(
-//       `https://swapi.dev/api/people/${term}/`
+// mousedown$.pipe(
+//   /*
+//    * In this case, we are mapping to a new interval
+//    * observable on mousedown, but we are limiting it's
+//    * lifetime by using the takeUntil operator with
+//    * the mouseup$ stream.
+//    */
+//   mergeMap(
+//     () => interval$.pipe(
+//       takeUntil(mouseup$)
 //     )
-//   }), 
-//   debounceTime(1000), 
-//   mergeAll()
+//   )
 // ).subscribe(console.log)
 
 
-input$.pipe(
-  debounceTime(1000), 
-  mergeMap((event: any) => {
-    const term = event.target.value
 
-    return ajax.getJSON(
-      `https://swapi.dev/api/people/${term}/`
-    )
-  })
-).subscribe(console.log)
+const coordinates$ = click$.pipe(
+  map((event: any) => ({
+    x: event.clientX,
+    y: event.clientY
+  }))
+);
+
+const coordinatesWithSave$ = coordinates$.pipe(
+  /*
+   * mergeMap is good for 'fire and forget' save request
+   * you do not want to be cancelled. For instance, in this
+   * example we are emulating a save of coordinates
+   * anytime the user clicks on the page.
+   */
+  mergeMap(coords => ajax.post(
+    'https://www.mocky.io/v2/5185415ba171ea3a00704eed'
+  ))
+);
+
+coordinatesWithSave$.subscribe(console.log);
