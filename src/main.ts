d6ff3057078ec4aaa12ab5d436d92f29490ccdaa
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-09 23:16:20 +0000

    Ref(*):combineLatest - latest values from multiple

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,59 @@
 
 
-import { EMPTY, fromEvent, interval, map, merge, scan, startWith, switchMap, takeWhile } from 'rxjs';
+import { combineLatest, filter, fromEvent, map } from 'rxjs';
 import './style.css'
 import { ajax } from 'rxjs/ajax';
 
- /*
- * CODE FOR FOR FIRST SECTION OF LESSON
- */
-// const keyup$ = fromEvent(document, 'keyup');
-// const click$ = fromEvent(document, 'click');
-
-// keyup$.subscribe(console.log);
-// click$.subscribe(console.log);
-
-/*
- * merge subscribes to all provided streams on subscription,
- * emitting any values emitted by these streams.
- */
-// merge(keyup$, click$).subscribe(console.log);
-
-
-/*
- * BEGIN SECOND SECTION OF LESSON
- */
-// elem refs
-const countdown: any = document.getElementById('countdown');
-const message: any= document.getElementById('message');
-const pauseButton : any= document.getElementById('pause');
-const startButton: any = document.getElementById('start');
+ // elems
+const first = document.getElementById('first');
+const second = document.getElementById('second');
 
 // streams
-const counter$ = interval(1000);
-const pauseClick$ = fromEvent(pauseButton, 'click');
-const startClick$ = fromEvent(startButton, 'click');
+const keyup$ = fromEvent(document, 'keyup');
+const click$ = fromEvent(document, 'click');
 
-const COUNTDOWN_FROM = 10;
+// helpers
+const keyupAsValue = (elem: any) => {
+  return fromEvent(elem, 'keyup').pipe(
+    map((event: any) => event.target.valueAsNumber)
+  );
+};
 
 /*
- * With merge, we can combine the start and pause
- * streams, taking relevant action below depending
- * on which stream emits a value.
+ * Each time any stream provided to combineLatest
+ * emits a value, the latest value from all provided
+ * streams will be emitted as an array. Note that all
+ * provided streams must emit at least one value before
+ * combineLatest will emit any values.
  */
-merge(
-  startClick$.pipe(map(() => true)), 
-  pauseClick$.pipe(map(() => false))
+// combineLatestWith(
+//   keyup$,
+//   click$
+// ).subscribe(console.log);
+
+/*
+ * When you want to augment one stream with 
+ * information from a second stream on emitted values,
+ * withLatestFrom is a perfect choice.
+ */
+// click$.pipe(
+//   withLatestFrom(interval(1000))
+// ).subscribe(console.log);
+
+/*
+ * combineLatest is great when an element depends
+ * on the combination of multiple streams to make
+ * some calculation or determination. We will explore
+ * this concept further in the next lab.
+ */
+combineLatest([
+  keyupAsValue(first), 
+  keyupAsValue(second)
+]
 )
 .pipe(
-  /*
-   * Depending on whether start or pause was clicked,
-   * we'll either switch to the interval observable,
-   * or to an empty observable which will act as a pause.
-   */
-  switchMap(shouldStart => {
-    return shouldStart ? counter$ : EMPTY;
+  filter(([first, second]) => {
+    return !isNaN(first) && !isNaN(second);
   }),
-
-  scan((accumulator, current) => {
-    return accumulator - 1;
-  }, COUNTDOWN_FROM),
-  takeWhile(value => value >= 0),
-  startWith(COUNTDOWN_FROM)
-)
-.subscribe(value => {
-  countdown.innerHTML = value;
-  if (!value) {
-    message.innerHTML = 'Liftoff!';
-  }
-});
+  map(([first, second]) => first + second)
+).subscribe(console.log);
