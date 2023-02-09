f915497d1674c3d996197cce1517b5cb999d9c05
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-09 12:38:13 +0000

    Ref(*): TypeAhead Search - switchMap

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,63 @@
 
-import { fromEvent, interval, map, mergeMap, takeUntil } from 'rxjs';
+import { debounceTime, distinctUntilChanged, fromEvent, interval, map, mergeMap, switchMap, takeUntil } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 import './style.css'
 
-const mousedown$ = fromEvent(document, 'mousedown')
-const mouseup$ = fromEvent(document, 'mouseup')
 
 
-const interval$ = interval(1000)
-const click$ = fromEvent(document, 'click');
+// /*
+//  * BEGIN FIRST SECTION
+//  */
+// const interval$ = interval(1000);
+// const click$ = fromEvent(document, 'click');
 
-// mousedown$.pipe(
+// click$.pipe(
 //   /*
-//    * In this case, we are mapping to a new interval
-//    * observable on mousedown, but we are limiting it's
-//    * lifetime by using the takeUntil operator with
-//    * the mouseup$ stream.
+//    * switchMap switches to a new observable on each emission
+//    * from the source, cancelling any previous inner 
+//    * observables. For instance, if you click once a new
+//    * interval observable will be subscribed to internally,
+//    * with it's values emitted. When you click again,
+//    * that observable will be completed, and the next 
+//    * interval will be subscribed to, restarting
+//    * the count. This will happen on each emission from
+//    * the click$ observable.
 //    */
-//   mergeMap(
-//     () => interval$.pipe(
-//       takeUntil(mouseup$)
-//     )
-//   )
-// ).subscribe(console.log)
+//   switchMap(() => interval$)
+// ).subscribe(console.log);
 
+/*
+ * BEGIN SECOND SECTION
+ */
+const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
+//elems
+const inputBox: any = document.getElementById('text-input');
+const typeaheadContainer: any = document.getElementById('typeahead-container');
 
-const coordinates$ = click$.pipe(
-  map((event: any) => ({
-    x: event.clientX,
-    y: event.clientY
-  }))
-);
+// streams
+const input$ = fromEvent(inputBox, 'keyup');
 
-const coordinatesWithSave$ = coordinates$.pipe(
-  /*
-   * mergeMap is good for 'fire and forget' save request
-   * you do not want to be cancelled. For instance, in this
-   * example we are emulating a save of coordinates
-   * anytime the user clicks on the page.
-   */
-  mergeMap(coords => ajax.post(
-    'https://www.mocky.io/v2/5185415ba171ea3a00704eed'
-  ))
-);
-
-coordinatesWithSave$.subscribe(console.log);
+input$
+  .pipe(
+    debounceTime(200),
+    map((event: any) => event.target.value),
+    distinctUntilChanged(),
+    /*
+     * switchMap is perfect for GET requests, as you do
+     * not normally care about the previous request
+     * to the same URL if another has fired. For instance,
+     * in this example if the user continues typing
+     * and the previuos request has not returned,
+     * switchMap will go ahead and cancel it and only
+     * the current request will be considered.
+     */
+    switchMap(searchTerm => ajax.getJSON(
+      `${BASE_URL}?by_name=${searchTerm}`
+      )
+    )
+  )
+  .subscribe((response: any) => {
+    // update ui
+    typeaheadContainer.innerHTML = response.map((b: any) => b.name).join('<br>');
+  });
