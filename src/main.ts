9d8738705b70b88411ec134babd1180c3ded653b
tom-kotlar <tomaskotlar@yahoo.co.uk>
2023-02-09 20:03:19 +0000

    Ref(*):Append values startWith, endWith

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,57 @@
 
 
-import { exhaustMap, finalize, fromEvent, map, mergeMap, pluck, switchMap, switchMapTo, takeUntil, tap, timer } from 'rxjs';
+import { endWith, fromEvent, interval, of, scan, startWith, takeUntil, takeWhile } from 'rxjs';
 import './style.css'
 import { ajax } from 'rxjs/ajax';
 
- const startButton: any = document.getElementById('start')
- const stopButton: any = document.getElementById('stop')
- const polingStatus: any = document.getElementById('polling-status')
- const dogImage: any = document.getElementById('dog');
+ const numbers$ = of(1,2,3);
 
- const startclick$ = fromEvent(startButton, 'click')
- const stopclick$ = fromEvent(stopButton, 'click')
-
- startclick$
- .pipe(
+numbers$.pipe(
   /*
-   * Every start click we will map to an interval which
-   * emits every 5 seconds to request a new image.
-   * Since we do not want multiple polls active at once,
-   * we'll use exhaustMap to ignore any emissions
-   * while the inner interval is running.
+   * startWith lets you seed a stream with 1:M values.
+   * On subscription, these values will be emitted
+   * immediately, followed by any future values from
+   * the source.
    */
-  exhaustMap(() =>
-    timer(0, 5000).pipe(
-      tap(() => (polingStatus.innerHTML = 'Active')),
-      switchMap(
-      () =>  ajax.getJSON('https://random.dog/woof.json', {'Content-Type':'application/json','Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': "GET"})),
-      /*
-       * Cancel the poll when stop click stream emits
-       */
-      takeUntil(stopclick$),
-      /*
-       * We'll use finalize to update the status to stopped
-       * each time the inner observable completes.
-       */
-      finalize(() => (polingStatus.innerHTML = 'Stopped'))
-    )
-  )
-)
-.subscribe(url => (dogImage.src = url));
+  startWith('a', 'b', 'c'),
+  /*
+   * You can also end a stream with any number of values,
+   * emitted on completion.
+   */
+  endWith('d', 'e', 'f')
+).subscribe(console.log);
 
+/*
+ * BEGIN SECOND SECTION OF LESSON
+ */
+// elem refs
+const countdown: any  = document.getElementById('countdown');
+const message: any  = document.getElementById('message');
+const abortButton: any = document.getElementById('abort');
+
+// streams
+const counter$ = interval(1000);
+const abort$ = fromEvent(abortButton, 'click');
+
+const COUNTDOWN_FROM = 10;
+
+counter$
+  .pipe(
+    scan((accumulator, current) => {
+      return accumulator  - 1 ;
+    }, COUNTDOWN_FROM),
+    takeWhile(value => value >= 0),
+    takeUntil(abort$),
+    /*
+     * With startWith, we can seed the stream with
+     * the starting countdown value.
+     */
+    startWith(COUNTDOWN_FROM)
+  )
+  .subscribe((value: any) => {
+    countdown.innerHTML = value;
+    if (!value) {
+      message.innerHTML = 'Liftoff!';
+    }
+  });
 
